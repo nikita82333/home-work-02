9b8f0eb1b50314fc586ca22337b9b68df30e792e
Кузнецов Никита Вениаминович <kunv@kbl.nsk.su>
2022-10-25 20:59:03 +0700

    correction defects 1.1

--- lib.h
+++ lib.h
@@ -7,11 +7,7 @@
 #include <initializer_list>
 #include <utility>
 
-using ip_table_int = std::vector<std::array<int, 4>>;
-using ip_table_str = std::vector<std::vector<std::string>>;
-
-//converting an ip pool from a string to an integer representation
-ip_table_int converter(const ip_table_str& ip_pool);
+using ip_table = std::vector<std::array<int, 4>>;
 
 // ("",  '.') -> [""]
 // ("11", '.') -> ["11"]
@@ -22,22 +18,22 @@ ip_table_int converter(const ip_table_str& ip_pool);
 std::vector<std::string> split(const std::string &str, char d);
 
 //descending sort
-void desc_sort(ip_table_int& ip_pool);
+void desc_sort(ip_table& ip_pool);
 
 //filter
 template<typename... T>
-ip_table_int filter(const ip_table_int& ip_pool, T... args)
+ip_table filter(const ip_table& ip_pool, T... args)
 {
     constexpr size_t MAX_ARGS_NUMBER = 4;
     std::vector<int> values;
-    ip_table_int result;
+    ip_table result;
     for (auto&& arg : std::initializer_list<int>{args...}) {
         values.push_back(arg);
     }
     for (const auto& ip : ip_pool) {
         bool condition = true;
         size_t i_args = 0;
-        for (auto value : values) {
+        for (const auto value : values) {
             if (value >= 0 && value <= 255 && i_args < MAX_ARGS_NUMBER) {
                 condition &= (value == ip.at(i_args));
             }
@@ -52,19 +48,19 @@ ip_table_int filter(const ip_table_int& ip_pool, T... args)
 
 //filter_any
 template<typename... T>
-ip_table_int filter_any(const ip_table_int& ip_pool, T... args)
+ip_table filter_any(const ip_table& ip_pool, T... args)
 {
     std::vector<int> values;
-    ip_table_int result;
+    ip_table result;
     for (auto&& arg : std::initializer_list<int>{args...}) {
         values.push_back(arg);
     }
     for (const auto& ip : ip_pool) {
         bool condition = true;
-        for (auto value : values) {
+        for (const auto value : values) {
             if (value >= 0 && value <= 255) {
                 bool condition_any = false;
-                for (const auto& ip_part : ip) {
+                for (const auto ip_part : ip) {
                     if (value == ip_part) {
                         condition_any = true;
                         break;
